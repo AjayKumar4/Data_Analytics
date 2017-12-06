00f5defcad29eab57c7dca940b60e1585cd02148
AJ <n.uajaykumar@gmail.com>
2017-12-06 21:22:57 +0000

    added data Visulaiztion for attrition
    added data Visulaiztion for attrition

--- Employee.R
+++ Employee.R
@@ -66,5 +66,105 @@ str(employee_dataset)
 describe(employee_dataset)
 summary(employee_dataset)
 
-#employeeCount, standard hours
+#employeeCount, standard hours, Over18,EmployeeNumber is not required for modelling 
+#since data does not mean anything to employee attribution column
+
+attrition_table <- table(dataset$Attrition)
+percentlabels<- round(100*attrition_table/sum(attrition_table),2)
+pielabels<- paste(percentlabels, "%", sep="")
+pie(attrition_table, labels=pielabels,main="Employee Attrition",col = c("green", "red"))
+legend("topright", names(attrition_table), cex=0.8, fill=c("green", "red"))
+
+employee_dataset %>%
+ggplot(aes(x= Attrition,  fill=Attrition)) + 
+  geom_bar(aes(y = ..count.., fill = factor(..x..)), stat="count") +
+  geom_text(aes( label = scales::percent((..count..)/sum(..count..)),
+                 y= ..prop.. ), stat= "count", vjust = -.5) +
+  labs(y = "Count", fill="day") +
+  theme(legend.position = "none", 
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) + 
+  ggtitle("Attrition")
+
+
+# Removing employeeCount, standard hours, Over18,EmployeeNumber columns(non-significant columns)
+
+employee_dataset$EmployeeNumber <- NULL
+employee_dataset$EmployeeCount <- NULL
+employee_dataset$EmployeeNumber <- NULL
+employee_dataset$StandardHours <- NULL
+employee_dataset$Over18 <- NULL
+
+str(employee_dataset)
+describe(employee_dataset)
+summary(employee_dataset)
+
+#Visulaiztion Considering Attrition of Employee
+
+#V1: Attrition VS Overtime
+employee_dataset %>%
+  ggplot(aes(x = OverTime, group = Attrition)) + 
+  geom_bar(aes(y = ..count.., fill = factor(..x..)), 
+           stat="count", 
+           alpha = 0.7) +
+  geom_text(aes(label = scales::percent(..prop..), y = ..prop.. ), 
+            stat= "count", 
+            vjust = -.5) +
+  labs(y = "Count", fill= "over time") +
+  facet_grid(~Attrition) +
+  theme(legend.position = "none", 
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) + 
+  ggtitle("Attrition VS Overtime",subtitle = "Attrition")
+
+#V2: BusinessTravel VS Attrition
+employee_dataset %>%
+  ggplot(aes(x = BusinessTravel, group = Attrition)) + 
+  geom_bar(aes(y = ..count.., fill = factor(..x..)), 
+           stat="count", 
+           alpha = 0.7) +
+  geom_text(aes(label = scales::percent(..prop..), y = ..prop.. ), 
+            stat= "count", 
+            vjust = -.5) +
+  labs(y = "Count", fill= "business travel") +
+  facet_grid(~Attrition) +
+  theme(legend.position = "none", 
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) + 
+  ggtitle("BusinessTravel VS Attrition",subtitle = "Attrition")
+
+#V3: Marital status VS Attrition
+employee_dataset %>%
+  ggplot(aes(x = MaritalStatus, group = Attrition)) + 
+  geom_bar(aes(y = ..count.., fill = factor(..x..)), 
+           stat="count", 
+           alpha = 0.7) +
+  geom_text(aes(label = scales::percent(..prop..), y = ..prop.. ), 
+            stat= "count", 
+            vjust = -.5) +
+  labs(y = "Count", fill= "marital status") +
+  facet_grid(~Attrition) +
+  theme(legend.position = "none", 
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) + 
+  ggtitle("Marital status VS Attrition",subtitle = "Attrition")
+
+#V4: Job role Vs Attrition
+levels(employee_dataset$JobRole) <- c("HC Rep", "HR", "LT", "Man", "MD", "RD", "RsScientist", "SalesEx", "SalesRep")
+employee_dataset %>%
+  ggplot(aes(x = JobRole, group = Attrition)) + 
+  geom_bar(aes(y = ..count.., fill = factor(..x..)), 
+           stat="count", 
+           alpha = 0.7) +
+  geom_text(aes(label = scales::percent(..prop..), y = ..prop.. ), 
+            stat= "count", 
+            vjust = -.5) +
+  labs(y = "Count", fill= "job role") +
+  facet_grid(~Attrition) +
+  theme(legend.position = "none", 
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) + 
+  ggtitle("Job role VS Attrition",subtitle = "Attrition")
+
+
 
--- Employee.py
+++ Employee.py
@@ -11,25 +11,34 @@ Created on Mon Nov 27 17:28:08 2017
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from ggplot import *
 
 # Importing the dataset
 dataset = pd.read_excel('Employeeattrition.xlsx')
-dataset['Education'] = dataset['Education'].replace([1, 2, 3, 4, 5], 
+
+employee_dataset = dataset
+employee_dataset['Education'] = employee_dataset['Education'].replace([1, 2, 3, 4, 5], 
                      ['Below College', 'College', 'Bachelor', 'Master', 'Doctor']) 
-dataset['EnvironmentSatisfaction'] = dataset['EnvironmentSatisfaction'].replace([1, 2, 3, 4], 
+employee_dataset['EnvironmentSatisfaction'] = employee_dataset['EnvironmentSatisfaction'].replace([1, 2, 3, 4], 
                      ['Low', 'Medium', 'High', 'Very High']) 
-dataset['JobInvolvement'] = dataset['JobInvolvement'].replace([1, 2, 3, 4], 
+employee_dataset['JobInvolvement'] = employee_dataset['JobInvolvement'].replace([1, 2, 3, 4], 
                      ['Low', 'Medium', 'High', 'Very High']) 
-dataset['JobSatisfaction'] = dataset['JobSatisfaction'].replace([1, 2, 3, 4], 
+employee_dataset['JobSatisfaction'] = employee_dataset['JobSatisfaction'].replace([1, 2, 3, 4], 
                      ['Low', 'Medium', 'High', 'Very High']) 
-dataset['PerformanceRating'] = dataset['PerformanceRating'].replace([1, 2, 3, 4], 
+employee_dataset['PerformanceRating'] = employee_dataset['PerformanceRating'].replace([1, 2, 3, 4], 
                      ['Low', 'Good', 'Excellent', 'Outstanding']) 
-dataset['RelationshipSatisfaction'] = dataset['RelationshipSatisfaction'].replace([1, 2, 3, 4], 
+employee_dataset['RelationshipSatisfaction'] = employee_dataset['RelationshipSatisfaction'].replace([1, 2, 3, 4], 
                      ['Low', 'Medium', 'High', 'Very High']) 
-dataset['WorkLifeBalance'] = dataset['WorkLifeBalance'].replace([1, 2, 3, 4], 
+employee_dataset['WorkLifeBalance'] = employee_dataset['WorkLifeBalance'].replace([1, 2, 3, 4], 
                      ['Bad', 'Good', 'Better', 'Best']) 
 
-dataset.describe()
+dataset_describe = employee_dataset.describe()
+
+ggplot(dataset,aes(x = 'Attrition', color = 'Attrition')) + geom_bar()
+
+#ggplot(aes(x='Attrition',color = 'Attrition'), data=dataset) + \
+#     geom_bar()
+
 header = list(dataset.columns.values)
 header.remove('Attrition')
 
